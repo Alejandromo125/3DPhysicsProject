f6b798cdece6ddd63ff6201ffd8e49227ff95e31
Alejandromo125 <alejandromartinortega@gmail.com>
2022-01-29 19:01:11 +0100

    Solved player direction

--- ModulePlayer.cpp
+++ ModulePlayer.cpp
@@ -54,7 +54,7 @@ bool ModulePlayer::Start()
 	float half_length = car.chassis_size.z*0.5f;
 	
 	vec3 direction(0,-1,0);
-	vec3 axis(1,0,0);
+	vec3 axis(-1,0,0);
 	
 	car.num_wheels = 4;
 	car.wheels = new Wheel[4];
@@ -111,8 +111,9 @@ bool ModulePlayer::Start()
 	vehicle->SetPos(0, 5, 0);
 
 	btQuaternion q;
-	q.setEuler(btScalar(180 * DEGTORAD), btScalar(0), btScalar(0));
-	vehicle->SetRotation(q);
+	//q.setEuler(btScalar(180 * DEGTORAD), btScalar(0), btScalar(0));
+	//vehicle->SetRotation()
+	//vehicle->SetRotation(q);
 	vehicle->collision_listeners.add(this);
 
 	//App->physics->AddConstraintP2P(*decorBody->body, *vehicle->body, car.rear_chassis_offset, car.rear_chassis_offset);
